94a381e5e889686dbc5d532897145be0b84e2c2e
정승현 <107786773+SeungHyun29@users.noreply.github.com>
2022-08-29 15:25:10 +0900

    Add: team-project first commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,48 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { BoardsModule } from './apis/boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ProductCategoryModule } from './apis/productsCategories/productsCategories.module';
+import { ProductModule } from './apis/products/products.module';
+import { UsersModule } from './apis/users/users.module';
+import { AuthsModule } from './apis/auths/auths.module';
+import { PointsTransactionsModule } from './apis/pointsTransactions/pointsTrnasactions.module';
+import { PaymentMoudle } from './apis/payment/payment.module';
+import { FilesModule } from './apis/files/file.module';
+import { AppController } from './commons/app.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthsModule,
+    BoardsModule,
+    FilesModule,
+    PointsTransactionsModule,
+    ProductCategoryModule,
+    ProductModule,
+    UsersModule,
+    PaymentMoudle,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+      context: ({ req, res }) => ({ req, res }),
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
+  controllers: [AppController],
+})
+export class AppModule {}
